6f933b39697ec84c35d63e2f89dbfca67d7b1b36
Jurgis Verbys <zipas162@gmail.com>
2021-10-25 23:35:13 +0300

    Add new course modal

--- assets/js/App.tsx
+++ assets/js/App.tsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "./components/Layout";
-import { Button, Card } from "react-bootstrap";
+import {Button, Card, Form, Modal} from "react-bootstrap";
 import CourseList from "./components/CourseList";
 
 const App = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <Layout>
             <Card>
                 <Card.Header className="d-flex justify-content-between align-items-center">
                     Courses
-                    <Button variant="primary" size="sm">
+                    <Button variant="primary" size="sm" onClick={handleShow}>
                         Create
                     </Button>
                 </Card.Header>
                 <Card.Body>
-                    <Card.Text>
-                        <CourseList />
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <CourseList />
                 </Card.Body>
             </Card>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>New course</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Control type="text" placeholder="Name" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Control type="text" placeholder="Description" />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </Layout>
     )
 };
